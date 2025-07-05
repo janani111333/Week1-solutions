3e3e7b61a9120085c4ea09157f980d1a11b7cada
janani111333 <vjanani112005@gmail.com>
2025-07-06 00:59:36 +0530

    Initial commit

---
+++ EF Core 8.0 HOL/RetailInventory/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+        "Server=localhost;Database=RetailInventoryDb;Trusted_Connection=True;TrustServerCertificate=True;",
+        options => options.EnableRetryOnFailure()
+    );
+
+    }
+}
