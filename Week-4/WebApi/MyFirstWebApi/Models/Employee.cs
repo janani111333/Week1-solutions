61405f832de15f7377dbc1858b2c8f65825c1825
vasanth <jamakayalavasanth@gmail.com>
2025-07-13 11:17:48 +0530

    Adding Week-4 Solutions

---
+++ Week-4/WebApi/MyFirstWebApi/Models/Employee.cs
@@ -0,0 +1,10 @@
+namespace MyFirstWebApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Salary { get; set; }
+        public string? Department { get; set; }
+    }
+}
